6e7ba2f8890a769942d73143f483694b7d57db03
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-12-04 21:15:23 -0800

    Tried out graphQL, getting it to work, ID is coming back as a string when it should be an integer

---
+++ back-end/src/db.js
@@ -0,0 +1,9 @@
+// export let users = [
+//     {id: 1, name: "Mack Weber", email: "mack@gmail.com", age: 27},
+//     {id: 2, name: "Miles Anderson", email: "miles@gmail.com", age: 25}
+// ]
+
+export let users = [
+    { id: 1, name: "John Doe", email: "john@gmail.com", age: 22 },
+    { id: 2, name: "Jane Doe", email: "jane@gmail.com", age: 23 }
+  ];
